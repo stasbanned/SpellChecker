f21f338cab303975be11aad072224d12b6de0920
stasbanned <stasbanned@gmail.com>
2018-05-13 14:33:56 +0300

    2nd commit

--- Vocabulary/ViewController.swift
+++ Vocabulary/ViewController.swift
@@ -10,33 +10,84 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var vocabulary = ["hello", "guys", "how", "are", "you", "aal", "aah", "aaa", "ara", "ata", "main", "mainly", "on", "the", "plain"]
-    var ar: [String] = []
-    var indexOfArray = -1
-    var key = -1
-    let alphabetArray = (97...122).map({Character(UnicodeScalar($0))})
-    
+    let vocabulary = ["hello", "guys", "how", "bow", "are", "you", "aal", "aah", "aaa", "ara", "ata", "main", "mainly", "on", "the", "plain", "was", "plaint", "in", "pain", "falls"]     //Custom vocabulary
+    let logicsObject = Logics()
+    var result: String = ""
+//    let vocabulary = Voc()     // Web vocabulary
     @IBOutlet weak var finishTextView: UITextView!
+    @IBOutlet weak var countOfWords: UITextField!
+    @IBOutlet weak var acticityIndicator: UIActivityIndicatorView!
     @IBOutlet weak var startText: UITextField!
     @IBOutlet weak var buttonCheck: UIButton!
     @IBAction func startTextAction(_ sender: Any) {
     }
     @IBAction func buttonCheckAction(_ sender: Any) {
-        var arrayOfStartText = startText.text?.components(separatedBy: " ")
+        acticityIndicator.startAnimating()
+        countOfWords.isHidden = false
+        let queue = DispatchQueue.global(qos: .utility)
+        queue.async{
+            self.result = self.logicsObject.result(startText: self.startText.text, vocabulary: self.vocabulary, t: self)
+                DispatchQueue.main.async {
+                    self.finishTextView.text = self.result
+                    self.acticityIndicator.stopAnimating()
+                }
+            }
+    }
+    
+    @IBAction func finishTextAction(_ sender: Any) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        countOfWords.isHidden = true
+    }
+}
+
+class Logics {
+    var finalString: String = ""
+    var ew = 0
+    var repeating = 0
+    var fullArray: [String] = []
+    var checkRepeatString = ""
+    var fullName: [String] = []
+    let alphabetArray = (97...122).map({Character(UnicodeScalar($0))})
+    var indexOfArray = -1
+    var countOfWords = 0
+    func result (startText: String?, vocabulary: [String], t: ViewController) -> String {
+        countOfWords = 0
+        finalString = ""
+        if repeating == 0 { ew = 0 }
+        if repeating == 0 {
+            checkRepeatString = ""
+        }
+        var key = -1
+        indexOfArray = -1
+        var arrayOfStartText = startText?.components(separatedBy: " ")
         first: for i in arrayOfStartText! {
+            countOfWords = 0
+            if repeating == 0 { ew = 0 }
             var string = ""
             indexOfArray += 1
             if vocabulary.contains(i) {
+                finalString += " " + i
                 continue first
             }
             var count: [String] = []
+            
             second: for _ in vocabulary {
+                countOfWords += 1
+                    DispatchQueue.main.async {
+                        t.countOfWords.text = "\(self.countOfWords) of \(vocabulary.count)"
+                    }
+
                 key = -1
                 for _ in Array(i) {
                     key += 1
                     var arrayOfCharsForDeleting: [Character] = Array(i)
                     arrayOfCharsForDeleting.remove(at: key)
                     let wordWithDeletedChar = String(arrayOfCharsForDeleting)
+                    if !fullArray.contains(wordWithDeletedChar) && repeating == 0 {
+                        fullArray.append(wordWithDeletedChar)
+                    }
                     if vocabulary.contains((wordWithDeletedChar)) {
                         if !count.contains(wordWithDeletedChar) {
                             if count.count == 0 {
@@ -46,14 +97,13 @@ class ViewController: UIViewController {
                                 count.append(wordWithDeletedChar)
                             }
                         }
-                        
-                        string = count.joined(separator: " ")
+                        string = count .joined(separator: " ")
                         if count.count > 1 {
                             string = "{" + count.joined(separator: " ") + "}"
                         }
                         arrayOfStartText![indexOfArray] = string
+                        ew = 1
                     }
-                    
                     var forLastIndexInserting = 0
                     third: for z in alphabetArray {
                         if (key + 1) == Array(i).count {
@@ -62,6 +112,9 @@ class ViewController: UIViewController {
                         var arrayOfCharsForInserting: [Character] = Array(i)
                         arrayOfCharsForInserting.insert(z, at: key)
                         let wordWithInsertedChar = String(arrayOfCharsForInserting)
+                        if !fullArray.contains(wordWithInsertedChar) && repeating == 0 {
+                            fullArray.append(wordWithInsertedChar)
+                        }
                         if vocabulary.contains(wordWithInsertedChar) {
                             if !count.contains(wordWithInsertedChar) {
                                 if count.count == 0 {
@@ -76,6 +129,7 @@ class ViewController: UIViewController {
                                 string = "{" + count.joined(separator: " ") + "}"
                             }
                             arrayOfStartText![indexOfArray] = string
+                            ew = 1
                         }
                         
                         if forLastIndexInserting == alphabetArray.count {
@@ -83,14 +137,18 @@ class ViewController: UIViewController {
                             for z in alphabetArray {
                                 var arrayOfCharsForInserting: [Character] = Array(i)
                                 arrayOfCharsForInserting.insert(z, at: key)
-                                let wordWithInsertedChar = String(arrayOfCharsForInserting)
-                                if vocabulary.contains(wordWithInsertedChar) {
-                                    if !count.contains(wordWithInsertedChar) {
+                                let wordWithInsertedsChar = String(arrayOfCharsForInserting)
+                                if !fullArray.contains(wordWithInsertedsChar) && repeating == 0 {
+                                    fullArray.append(wordWithInsertedsChar)
+                                }
+
+                                if vocabulary.contains(wordWithInsertedsChar) {
+                                    if !count.contains(wordWithInsertedsChar) {
                                         if count.count == 0 {
-                                            count.append(wordWithInsertedChar)
+                                            count.append(wordWithInsertedsChar)
                                         } else {
                                             count.append(" ")
-                                            count.append(wordWithInsertedChar)
+                                            count.append(wordWithInsertedsChar)
                                         }
                                     }
                                     string = count.joined(separator: " ")
@@ -98,28 +156,67 @@ class ViewController: UIViewController {
                                         string = "{" + count.joined(separator: " ") + "}"
                                     }
                                     arrayOfStartText![indexOfArray] = string
+                                    ew = 1
                                 }
                             }
                         }
                     }
                 }
             }
-            if string == "" {
-                string = "{" + i + "?}"
-                arrayOfStartText![indexOfArray] = string
-            }
             
+            if ew == 0 {
+                raz: for k in fullArray {
+                    if !vocabulary.contains(k) && repeating == 0 {
+                        repeating = 1
+                        result(startText: k, vocabulary: vocabulary, t: t)
+                        fullArray = []
+                        repeating = 0
+                    }
+                }
+            }
+            if string != "" {
+                ew = 1
+                fullArray = []
+            }
+            if string == "" && repeating == 0{
+                if ew == 0 {
+                    string = "{" + i + "?}"
+                    arrayOfStartText![indexOfArray] = string
+                }
+            }
+            if string != "" && repeating == 0 {
+                finalString += " " + string
+            }
+            if string != "" && repeating != 0 && string != checkRepeatString{
+                finalString += " " + string
+            }
+            if repeating != 0 && string != ""{
+                checkRepeatString = string
+            }
         }
-        
-        
-        
-        finishTextView.text = arrayOfStartText?.joined(separator: " ")
+        return finalString
     }
-    @IBAction func finishTextAction(_ sender: Any) {
+    
+    func down(c: Int) -> String {
+        let a = ViewController()
+        return "\(c) of \(a.vocabulary.count)"
     }
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-
+    
 }
 
+class Voc {
+    func voc() -> [String] {
+        var fullName: [String] = []
+        if let url = URL(string: "https://docs.oracle.com/javase/tutorial/collections/interfaces/examples/dictionary.txt") {
+            do {
+                let contents = try String(contentsOf: url)
+                fullName = contents.components(separatedBy: "\n")
+            } catch {
+                // contents could not be loaded
+            }
+        } else {
+            // the URL was bad!
+        }
+        return fullName
+    }
+}
